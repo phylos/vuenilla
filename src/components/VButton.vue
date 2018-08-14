1e063814137838a14e49679f908668236d9757a3
Tyler King <doitliketyler@gmail.com>
2018-08-14 13:57:29 -0700

    Added some button props

--- src/components/VButton.vue
+++ src/components/VButton.vue
@@ -1,11 +1,14 @@
 <template>
-  <a class="v-button">
-    <span class="v-button-text"><slot/></span>
-  </a>
+  <div :class="'v-button ' + size + ' ' + color">
+    <a :href="href" class="v-button-link">
+      <span class="v-button-text"><slot/></span>
+    </a>
+  </div>
 </template>
 
 <script>
   export default {
+    props: ['href', 'color', 'size'],
     data () {
       return {
       }
